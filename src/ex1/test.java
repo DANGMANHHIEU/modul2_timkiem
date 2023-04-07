3a0714ee6d98f7a6dc654dfa749d7774475708bd
DANGMANHHIEU <manhhieu261296@gmail.com>
2023-04-07 10:48:30 +0700

    timkiem

---
+++ src/ex1/test.java
@@ -0,0 +1,9 @@
+package ex1;
+
+public class test {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+        System.out.println(binarySearch.search(arr,6));
+        System.out.println(binarySearch.search(arr,10));
+    }
+}
