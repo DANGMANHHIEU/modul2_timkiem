5a537bfb0d2cc97eb5a74cf0e4b83bcbf98be8b7
DANGMANHHIEU <manhhieu261296@gmail.com>
2023-04-07 14:38:44 +0700

    tim kiem

---
+++ src/bai1/maxTest.java
@@ -0,0 +1,11 @@
+package bai1;
+
+import java.util.Scanner;
+
+public class maxTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str= sc.nextLine();
+        max.checkMax(str);
+    }
+}
